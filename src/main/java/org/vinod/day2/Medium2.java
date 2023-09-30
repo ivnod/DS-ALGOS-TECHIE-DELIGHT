5ff93593473a6617eeeca23204c00cf337048b4d
VInod <vinodreddymedagam@gmail.com>
2023-09-30 21:02:11 +0800

    Medium 2 done

--- src/main/java/org/vinod/day2/Medium2.java
+++ src/main/java/org/vinod/day2/Medium2.java
@@ -1,37 +1,33 @@
 package org.vinod.day2;
-// Find the index of 0 to be replaced to get the maximum length sequence of continuous ones in
-// binary array
+
+import java.util.Arrays;
+
+// Print continuous subarray with maximum sum
 public class Medium2 {
   public static void main(String[] args) {
-    int[] A = {0, 0, 1, 0, 1, 1, 1, 0, 1, 1};
+    int[] arr = {8, -7, -3, 5, 6, -2, 3, -4, 2};
 
-    int index = findIndexofZero(A);
-
-    if (index != -1) {
-      System.out.print("Index to be replaced is " + index);
-    } else {
-      System.out.print("Invalid input");
-    }
+    System.out.println(
+        "The maximum sum of a contiguous subarray is " + Arrays.toString(kadaneNeg(arr)));
   }
 
-  private static int findIndexofZero(int[] A) {
-    int count = 0;
-    int max_count = 0;
-    int max_index = -1;
-    int prev_zero_index = -1;
-    for (int i = 0; i < A.length; i++) {
-      if (A[i] == 1) {
-        count++;
-      } else {
-        count = i - prev_zero_index;
-        prev_zero_index = i;
-        if (count > max_count) {
-          max_count = count;
-          max_index = prev_zero_index;
-        }
+  private static int[] kadaneNeg(int[] arr) {
+    int max_so_far = Integer.MIN_VALUE;
+    int max_ending_here = 0;
+    int start = 0, end = 0;
+    int beg = 0;
+    for (int i = 0; i < arr.length; i++) {
+      max_ending_here = max_ending_here + arr[i];
+      if (max_ending_here < arr[i]) {
+        max_ending_here = arr[i];
+        beg = i;
+      }
+      if (max_so_far < max_ending_here) {
+        max_so_far = max_ending_here;
+        start = beg;
+        end = i;
       }
     }
-
-    return max_index;
+    return Arrays.copyOfRange(arr, start, end + 1);
   }
 }
