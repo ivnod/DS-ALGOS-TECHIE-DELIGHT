0516c8abd2a5d9d1716a912acec9e8c766393805
VInod <vinodreddymedagam@gmail.com>
2023-10-02 12:17:30 +0800

    Easy completed

--- src/main/java/org/vinod/day4/Easy.java
+++ src/main/java/org/vinod/day4/Easy.java
@@ -5,5 +5,26 @@ import java.util.Map;
 
 // Reverse a string using recursion
 public class Easy {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String str = "Techie Delight";
+
+    char[] c = str.toCharArray();
+    reverse(c, 0, c.length - 1);
+    str = new String(c);
+
+    System.out.print("Reverse of the given string is " + str);
+  }
+
+  private static void reverse(char[] c, int l, int h) {
+    if (l < h) {
+      swap(c, l, h);
+      reverse(c, l + 1, h - 1);
+    }
+  }
+
+  private static void swap(char[] c, int i, int j) {
+    char temp = c[i];
+    c[i] = c[j];
+    c[j] = temp;
+  }
 }
--- src/main/java/org/vinod/day4/Easy1.java
+++ src/main/java/org/vinod/day4/Easy1.java
@@ -1,6 +1,53 @@
 package org.vinod.day4;
 
-// Implement Binary Search  Java Collections
+// Implement Binary Search  Java arrays
 public class Easy1 {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    int[] nums = {2, 5, 6, 8, 9, 10};
+    int target = 5;
+
+    int index = binarySearchIterative(nums, target);
+
+    if (index != -1) {
+      System.out.println("Element found at index " + index);
+    } else {
+      System.out.println("Element not found in the array");
+    }
+
+    int left = 0;
+    int right = nums.length - 1;
+
+    int indexRecursive = binarySearchRecursive(nums, left, right, target);
+
+    if (indexRecursive != -1) {
+      System.out.println("Element found at index " + indexRecursive);
+    } else {
+      System.out.println("Element not found in the array");
+    }
+  }
+
+  private static int binarySearchRecursive(int[] nums, int left, int right, int target) {
+    if (left > right) return -1;
+    int mid = (left + right) / 2;
+    if (nums[mid] == target) return mid;
+    else if (target < nums[mid]) return binarySearchRecursive(nums, left, mid - 1, target);
+    else return binarySearchRecursive(nums, mid + 1, right, target);
+  }
+
+  private static int binarySearchIterative(int[] nums, int target) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (target == nums[mid]) {
+        return mid;
+      } else if (target > nums[mid]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+
+    return -1;
+  }
 }
